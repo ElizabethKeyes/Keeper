69ac74d61b39679bd685ceec050f321b859992ba
Elizabeth Keyes <liz4momentum@gmail.com>
2023-05-06 14:44:16 -0600

    Masonry look achieved, only for desktop

--- Keeper.client/src/pages/HomePage.vue
+++ Keeper.client/src/pages/HomePage.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="container-fluid">
-    <section class="row">
-      <div v-for="k in keeps" :key="k.id" class="col-6 col-md-3">
-        <div class="keeps-card">
+    <section class="row mt-3">
+      <div class="col-3">
+        <div v-for="k in keeps1[1]" :key="k.id" class="keeps-card">
+          <img :src="k.img" :alt="'a photo of ' + k.name" class="keeps-img">
+        </div>
+      </div>
+      <div class="col-3">
+        <div v-for="k in keeps1[2]" :key="k.id" class="keeps-card">
+          <img :src="k.img" :alt="'a photo of ' + k.name" class="keeps-img">
+        </div>
+      </div>
+      <div class="col-3">
+        <div v-for="k in keeps1[3]" :key="k.id" class="keeps-card">
+          <img :src="k.img" :alt="'a photo of ' + k.name" class="keeps-img">
+        </div>
+      </div>
+      <div class="col-3">
+        <div v-for="k in keeps1[4]" :key="k.id" class="keeps-card">
           <img :src="k.img" :alt="'a photo of ' + k.name" class="keeps-img">
         </div>
       </div>
     </section>
+    {{ keeps1 }}
   </div>
 </template>
 
@@ -35,6 +51,28 @@ export default {
 
     return {
       keeps: computed(() => AppState.keeps),
+      keeps1: computed(() => {
+        const length = AppState.keeps.length
+        const keeps = {
+          1: [],
+          2: [],
+          3: [],
+          4: []
+        }
+        for (let i = 0; i < (length); i++) {
+          if (i < length / 4) {
+            keeps[1].push(AppState.keeps[i])
+          } else if (i < length / 2) {
+            keeps[2].push(AppState.keeps[i])
+          } else if (i < length / 1.33) {
+            keeps[3].push(AppState.keeps[i])
+          } else {
+            keeps[4].push(AppState.keeps[i])
+          }
+        }
+        logger.log('[KEEPS OBJECT]', keeps)
+        return keeps
+      }),
 
     }
   }
@@ -44,12 +82,14 @@ export default {
 <style scoped lang="scss">
 .keeps-card {
   border-radius: 5px;
+  margin-bottom: 1em;
 }
 
 .keeps-img {
   object-fit: cover;
   object-position: center;
   width: 100%;
+  border-radius: 5px;
 
 }
 </style>
